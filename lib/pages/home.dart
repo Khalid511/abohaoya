3dc57ed4de29400525a137149bc880e5b8f57613
Md. Khalid Hasan <shohag9438@gmail.com>
2022-11-11 18:28:59 +0600

    move app bar title to center

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,9 +10,10 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(child:
       Scaffold(
+
         appBar: AppBar(
-          title: Text('Weather Dekho',
-          textAlign: TextAlign.center,),
+          title: Text('Weather Dekho',),
+          centerTitle: true,
 
         ),
       ),
