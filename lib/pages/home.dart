a73a819b541afa574f540d11bccbae7beb14f0ae
Md. Khalid Hasan <shohag9438@gmail.com>
2022-11-19 10:43:41 +0600

    text color is changed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -172,7 +172,7 @@ class _HomeState extends State<Home> {
                               style: TextStyle(
                                 fontSize: 22.0,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.blueGrey
+                                color: Colors.red.shade400
                               ),
                             ))
                       ],
