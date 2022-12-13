e7ccb5988178fe4c211dab047d68542542bcea54
Md. Khalid Hasan <shohag9438@gmail.com>
2022-12-13 17:19:18 +0600

    add properties to bottom list

--- lib/pages/ForecastList.dart
+++ lib/pages/ForecastList.dart
@@ -11,8 +11,43 @@ class ForecastList extends StatelessWidget {
         Container(
           width: 150,
          // color: Colors.yellow,
-          child: Text(
-            "Sun"
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Sun",
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Min / Max",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(onPressed: (){
+
+              },
+                  child: Text(
+                    "View",
+                  ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.yellow,
+                ),
+              ),
+            ],
           ),
           decoration: BoxDecoration(
             color: Colors.yellow,
@@ -21,8 +56,46 @@ class ForecastList extends StatelessWidget {
         ),
         Container(
           width: 150,
-          child: Text(
-            "Mon",
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Mon",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Min / Max",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.limeAccent,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(onPressed: (){
+
+              },
+                child: Text(
+                  "View",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                ),
+              ),
+            ],
           ),
           decoration: BoxDecoration(
             color: Colors.purple,
@@ -31,8 +104,43 @@ class ForecastList extends StatelessWidget {
         ),
         Container(
           width: 150,
-          child: Text(
-            "Tue",
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Tue",
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Min / Max",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(onPressed: (){
+
+              },
+                child: Text(
+                  "View",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                ),
+              ),
+            ],
           ),
           decoration: BoxDecoration(
             color: Colors.red,
@@ -41,8 +149,46 @@ class ForecastList extends StatelessWidget {
         ),
         Container(
           width: 150,
-          child: Text(
-            "Wed",
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Wed",
+                style: TextStyle(
+                  color: Colors.teal,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Min / Max",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(onPressed: (){
+
+              },
+                child: Text(
+                  "View",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                ),
+              ),
+            ],
           ),
           decoration: BoxDecoration(
             color: Colors.orange,
@@ -51,8 +197,46 @@ class ForecastList extends StatelessWidget {
         ),
         Container(
           width: 150,
-          child: Text(
-            "Thu",
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Thu",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Min / Max",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.yellowAccent,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(onPressed: (){
+
+              },
+                child: Text(
+                  "View",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                ),
+              ),
+            ],
           ),
           decoration: BoxDecoration(
             color: Colors.blue,
@@ -61,24 +245,66 @@ class ForecastList extends StatelessWidget {
         ),
         Container(
           width: 150,
-          child: Text(
-            "Fri",
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Fri",
+                style: TextStyle(
+                  color: Colors.yellowAccent,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Min / Max",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "Description",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ElevatedButton(onPressed: (){
+
+              },
+                child: Text(
+                  "View",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+              ),
+            ],
           ),
           decoration: BoxDecoration(
             color: Colors.green,
             borderRadius: BorderRadius.all(Radius.circular(100)),
           ),
         ),
-        Container(
-          width: 150,
-          child: Text(
-            "Sat",
-          ),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.all(Radius.circular(100)),
-          ),
-        ),
+        // Container(
+        //   width: 150,
+        //   child: Text(
+        //     "Sat",
+        //     style: TextStyle(
+        //       color: Colors.white,
+        //     ),
+        //     textAlign: TextAlign.center,
+        //   ),
+        //   decoration: BoxDecoration(
+        //     color: Colors.black,
+        //     borderRadius: BorderRadius.all(Radius.circular(100)),
+        //   ),
+        // ),
       ],
     );
   }
